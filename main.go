eada975e35887013a40e25120505b136d945dc13
Dalton McGee <dalton.mc@gmail.com>
2022-01-17 22:23:11 -0600

    Refactor a bit, add comments

--- main.go
+++ main.go
@@ -41,9 +41,7 @@ func handle404(w http.ResponseWriter) {
 
 func handleUri(w http.ResponseWriter, r *http.Request) {
 	templateHome := "./templates/home.html"
-	type IndexData struct {
-		Title string
-	}
+
 	path := r.URL.Path
 
 	// Checking for pattern used for dynamic pages and return 404 if found.
@@ -54,57 +52,74 @@ func handleUri(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if strings.Index(path, "/static/") != 0 {
-		if path == "/" {
-			files := append([]string{"./public/index.html"}, templateHome)
-			t, _ := template.ParseFiles(files...)
+	if path == "/" {
+		files := append([]string{"./public/index.html"}, templateHome)
+		t, _ := template.ParseFiles(files...)
+		t.Execute(w, nil)
+	} else {
+		files := append([]string{fmt.Sprintf("./public%s.html", path)}, templateHome)
+		t, err := template.ParseFiles(files...)
+
+		if err == nil {
 			t.Execute(w, nil)
-		} else if path != "/favicon.ico" {
-			files := append([]string{fmt.Sprintf("./public%s.html", path)}, templateHome)
-			t, err := template.ParseFiles(files...)
-			if err == nil {
-				t.Execute(w, nil)
-			} else {
-				fileName := strings.Split(path, "/")
-				queryableValue := &fileName[len(fileName)-1]
-				directory := strings.Join(fileName[:len(fileName)-1], "/")
-				directoryFiles, err := ioutil.ReadDir(fmt.Sprintf("./public/%s", directory))
-				if err != nil {
-					handle404(w)
-					return
-				}
-				for _, file := range directoryFiles {
-					if !file.IsDir() {
-						isFile, _ := regexp.Match(`\[\w+\]`, []byte(file.Name()))
-						if isFile {
-							jsonBytes, err := loadFile("./noSQL.json")
-							if err != nil {
-								handle404(w)
-								return
-							}
+		} else {
 
-							jsonMap := map[string][]interface{}{}
-							queryKey := regexp.MustCompile(`\[|\]`).Split(file.Name(), -1)[1]
+			// Take the URI, strip off the last data to get the directory, then
+			// get a list of all files in the directory to be looped over.
+			// If the directory doesn't exist throw a 404.
+			fileName := strings.Split(path, "/")
+			queryableValue := &fileName[len(fileName)-1]
+			directory := strings.Join(fileName[:len(fileName)-1], "/")
+			directoryFiles, err := ioutil.ReadDir(fmt.Sprintf("./public/%s", directory))
 
-							json.Unmarshal([]byte(jsonBytes), &jsonMap)
-
-							for _, val := range jsonMap["data"] {
-								for key, value := range val.(map[string]interface{}) {
-									if key == queryKey && *queryableValue == value {
-										fullDirectory := fmt.Sprintf("./public%s/%s", directory, file.Name())
-										files := append([]string{fullDirectory}, templateHome)
-										t, _ := template.ParseFiles(files...)
-										t.Execute(w, val)
-										return
-									}
-								}
-							}
-						}
-					}
-				}
+			if err != nil {
 				handle404(w)
 				return
 			}
+
+			// Loop over all files in the directory and see if the template name matches
+			// any of the keys in the JSON data provided. If so, serve the first one found.
+			for _, file := range directoryFiles {
+
+				// Skip subdirectories.
+				if file.IsDir() {
+					continue
+				}
+
+				// Skip file if it doesn't match the template format.
+				isFile, _ := regexp.Match(`\[\w+\]`, []byte(file.Name()))
+
+				if !isFile {
+					continue
+				}
+
+				jsonBytes, err := loadFile("./noSQL.json")
+
+				if err != nil {
+					// This should probably throw a different error
+					handle404(w)
+					return
+				}
+
+				jsonMap := map[string][]interface{}{}
+				queryKey := regexp.MustCompile(`\[|\]`).Split(file.Name(), -1)[1]
+
+				json.Unmarshal([]byte(jsonBytes), &jsonMap)
+
+				for _, val := range jsonMap["data"] {
+					for key, value := range val.(map[string]interface{}) {
+						if key == queryKey && *queryableValue == value {
+							fullDirectory := fmt.Sprintf("./public%s/%s", directory, file.Name())
+							files := append([]string{fullDirectory}, templateHome)
+							t, _ := template.ParseFiles(files...)
+							t.Execute(w, val)
+							return
+						}
+					}
+				}
+			}
+			handle404(w)
+			return
 		}
 	}
 	return
@@ -119,8 +134,11 @@ func handleStatic(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func doNothing(w http.ResponseWriter, r *http.Request) {}
+
 func main() {
 	http.HandleFunc("/", handleUri)
 	http.HandleFunc("/static/", handleStatic)
+	http.HandleFunc("/favicon.ico", doNothing)
 	log.Fatal(http.ListenAndServe(":3010", nil))
 }
