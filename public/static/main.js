da0574c26256fa87dfc467392de37573bada9f13
Dalton McGee <dalton.mc@gmail.com>
2022-02-08 22:12:30 -0600

    add transition to desktop

--- public/static/main.js
+++ public/static/main.js
@@ -1,1 +1,17 @@
-console.log("hello, multiverse");
+const main = document.querySelector('main');
+const bodyItems = document.querySelectorAll('.col');
+
+main.addEventListener('scroll', ()=>{
+  for (let element of bodyItems){
+    let boundingRect = element.getBoundingClientRect();
+    let content = element.querySelector('.content');
+
+    // check to see if the center of the <main> element
+    // is within the center of any given element
+    if (boundingRect.left < main.clientWidth && main.clientWidth < boundingRect.right) {
+      content.classList.add('active');
+    } else {
+      content.classList.remove('active');
+    }
+  }
+})
