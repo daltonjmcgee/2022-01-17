26c269c965c7e355f004f15c29f626630e8e5459
Dalton McGee <dalton.mc@gmail.com>
2022-02-14 21:51:57 -0600

    refactor for vimeo embed instead of img

--- public/static/main.js
+++ public/static/main.js
@@ -1,17 +1,52 @@
-const main = document.querySelector('main');
-const bodyItems = document.querySelectorAll('.col');
+function contentHandler(main) {
 
-main.addEventListener('scroll', ()=>{
-  for (let element of bodyItems){
-    let boundingRect = element.getBoundingClientRect();
-    let content = element.querySelector('.content');
+  function contentDisplayHandler() {
+    const bodyItems = document.querySelectorAll('.col');
+    for (let element of bodyItems) {
+      let boundingRect = element.getBoundingClientRect();
+      let content = element.querySelector('.content');
 
-    // check to see if the center of the <main> element
-    // is within the center of any given element
-    if (boundingRect.left < main.clientWidth && main.clientWidth < boundingRect.right) {
-      if (!content.classList.contains('active')) content.classList.add('active');
-    } else {
-      if (content.classList.contains('active')) content.classList.remove('active');
+      // check to see if the center of the <main> element
+      // is within the center of any given element
+      if (boundingRect.left < main.clientWidth && main.clientWidth < boundingRect.right) {
+        if (!content.classList.contains('active')) content.classList.add('active');
+      } else {
+        if (content.classList.contains('active')) content.classList.remove('active');
+      }
     }
   }
+
+  // Init on page load
+  contentDisplayHandler();
+
+  main.addEventListener('scroll', () => {
+    contentDisplayHandler();
+  })
+}
+
+function arrowNavigationHandler(main) {
+  function handleClick(direction) {
+    const element = document.querySelector(`.${direction}`)
+    element.addEventListener('click', () => {
+      const sibling = document.querySelector('.active').parentNode[`${direction}ElementSibling`];
+      if (!!sibling && sibling.classList.contains('col')) {
+        main.scroll({
+          left: sibling.offsetLeft - sibling.clientWidth,
+          behavior: 'smooth'
+        })
+      }
+    })
+  }
+
+  handleClick("next");
+  handleClick("previous");
+}
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const main = document.querySelector('main');
+  contentHandler(main);
+  arrowNavigationHandler(main)
 })
+
